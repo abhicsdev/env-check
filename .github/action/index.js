315036b1f48db565c01fe9031f928a9f908577e1
abhicsdev <70186380+abhicsdev@users.noreply.github.com>
2023-01-17 14:00:31 +0530

    update

--- .github/action/index.js
+++ .github/action/index.js
@@ -1,13 +1,24 @@
 let ADMINS = process.env.ADMINS;
 
+let checkUser = (str) => {
+  if(ADMINS.includes(str) === true) {
+    return true;
+  }
+  return false;
+}
+
 async function fun() {
   console.log(process.env);
-  console.log(ADMINS);
   
-  if(ADMINS.includes("abhin1509") === true) {
-    console.log("working");
-  } else {
-    console.log("not");
-  }
+  console.log("abhin1509:: ", checkUser("abhin1509"));
+  console.log("akki:: ", checkUser("akki"));
+  console.log("ceejeey:: ", checkUser("ceejeey"));
+  console.log("soham:: ", checkUser("soham"));
+  console.log("kumar:: ", checkUser("kumar"));
+  console.log("abhi:: ", checkUser("abhi"));
+  console.log("abhin150:: ", checkUser("abhin150"));
+  console.log("ceejey:: ", checkUser("ceejey"));
+  console.log("akkivc:: ", checkUser("abhin1509"));
+  console.log("soha:: ", checkUser("soha"));
 }
 fun();
